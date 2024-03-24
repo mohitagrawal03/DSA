24576d76a949576a964d8df9ca01f8758706d297
Mohit Agrawal <115724835+mohitagrawal03@users.noreply.github.com>
2024-03-24 20:10:26 +0530

    Create Q002_LinearSearch.java

---
+++ 001 Arrays /002 Linear Search /Q002_LinearSearch.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class lec10_array_LinearSearch{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of an Array: ");
+        int length = sc.nextInt();
+
+        int[] arr = new int[length];
+        System.out.println("Enter elements:");
+        for(int i = 0; i < length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the no. to search: ");
+        int search_element = sc.nextInt();
+        int found = 0;
+        for(int i = 0; i < length; i++){
+            if(search_element == arr[i]){
+                System.out.println("Element is found at index: "+i);
+                found = 1;
+            }
+        }
+        if(found == 0){
+            System.out.println("Element not found!");
+        }
+    }
+}
